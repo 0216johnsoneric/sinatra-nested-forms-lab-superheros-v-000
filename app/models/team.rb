94e345fb6d158cc07a44661c2b70550e832f1fcb
Eric Johnson <ericjohnson0216@icloud.com>
2020-05-10 18:38:42 +0000

    Automatically backed up by Learn

--- app/models/team.rb
+++ app/models/team.rb
@@ -1,23 +1,8 @@
-# class Team
-#   attr_reader :name, :motto
-#
-#   def initialize(params)
-#     @name = params[:name]
-#     @motto = params[:motto]
-#   end
-# end
 class Team
   attr_accessor :name, :motto
-
-  TEAMS = []
 
   def initialize(params)
     @name = params[:name]
     @motto = params[:motto]
-    @@all << self
-  end
-
-  def self.all
-    TEAMS
   end
 end
