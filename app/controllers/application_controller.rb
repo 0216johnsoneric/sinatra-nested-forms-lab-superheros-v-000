7e8198250453acdfb7e61ed1e8fef40b316834f9
Eric Johnson <ericjohnson0216@icloud.com>
2020-05-10 18:36:13 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,28 +8,10 @@ class App < Sinatra::Base
       erb :super_hero
     end
 
-    post '/teams' do
-      @team = Team.new(name: params[:team][:name], motto: params[:team][:motto])
-      params[:team][:heroes].each {|hero| Superhero.new(hero)}
-      @heroes=Superhero.all
-    end
-
     # post '/teams' do
-    #   members = params[:team][:members]
     #   @team = Team.new(name: params[:team][:name], motto: params[:team][:motto])
-    #
-    #   members.each do |member_params|
-    #     SuperHero.new({name: member_params[:name], power: member_params[:power], bio: member_params[:bio]})
-    #   end
-    #   @super_heroes = SuperHero.all
-    #   erb :team
-    # end
-
-    # post '/teams' do
-    #   @team = Team.new({name: params[:team][:name], motto: params[:team][:motto]})
     #   params[:team][:heroes].each {|hero| Superhero.new(hero)}
     #   @heroes=Superhero.all
-    #
-    #   erb :team
     # end
+
 end
